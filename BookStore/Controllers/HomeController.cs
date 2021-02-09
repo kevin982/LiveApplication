dd4235e586e0bd07b068db1837f50629e7a6dde0
kevin989 <kevinvb612@gmail.com>
2021-02-09 13:28:30 -0600

    A layout was created and implemented

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -11,7 +11,7 @@ namespace BookStore.Controllers
     {
         public ViewResult Index()
         {
-            return View("Index");
+            return View();
         }
 
         public ViewResult Aboutus()
--- BookStore/Startup.cs
+++ BookStore/Startup.cs
@@ -17,11 +17,17 @@ namespace BookStore
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+#if DEBUG
+            services.AddRazorPages().AddRazorRuntimeCompilation();
+#endif
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
