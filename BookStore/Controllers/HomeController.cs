641afc162bfb12415919575b771c8423c113b22b
kevin989 <kevinvb612@gmail.com>
2021-02-11 06:21:20 -0600

    ViewData was applied.

--- BookStore/Controllers/HomeController.cs
+++ BookStore/Controllers/HomeController.cs
@@ -4,11 +4,15 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using BookStore.Models;
 
 namespace BookStore.Controllers
 {
     public class HomeController:Controller
     {
+        [ViewData]
+        public string Title { get; set; }
+
         public ViewResult Index()
         {
             return View();
