ce69af0c871345acca2c8c77a93a0e2063293738
kevin989 <kevinvb612@gmail.com>
2021-02-24 10:41:09 -0600

    The form to add a new user has been added. Besides  we have added a control to manage the security of our application.

---
+++ BookStore/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using BookStore.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Controllers
+{
+    
+    [Route("[controller]/[action]")]
+    public class AccountController : Controller
+    {
+
+        public IActionResult SignUp()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult SignUp(SignUpUserModel userModel)
+        {
+
+            if (ModelState.IsValid)
+            {
+
+            }
+
+            return View();
+        }
+    }
+}
